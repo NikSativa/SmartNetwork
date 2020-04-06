75bbe806f605f1e3a35fbd27b5e14316b1a5d81d
Nikita Konopelko <nik.sativa@gmail.com>
2020-04-06 17:21:45 +0300

    falbcks removed

--- Source/Test/URLRequest+TestHelper.swift
+++ Source/Test/URLRequest+TestHelper.swift
@@ -1,10 +1,20 @@
 import Foundation
 import Spry
+import Nimble
+import Spry_Nimble
 
 import NRequest
 
-extension URLRequest {
-    public static func testMake(url: URL = .testMake()) -> URLRequest {
-        return URLRequest(url: url)
+extension URLRequest: SpryEquatable, TestOutputStringConvertible {
+    public static func testMake(url: URL = .testMake(), headers: [String: String] = [:]) -> URLRequest {
+        var request = URLRequest(url: url)
+        for (key, value) in headers {
+            request.addValue(value, forHTTPHeaderField: key)
+        }
+        return request
+    }
+
+    public var testDescription: String {
+        [String(describing: type(of: self)), self.description, String(describing: allHTTPHeaderFields)].compactMap({ $0 }).joined(separator: ", ")
     }
 }
