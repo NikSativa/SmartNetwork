759b3f374d36f05e9176db6d7d535288898a8ee2
Mykyta Konopelko <nik.sativa@gmail.com>
2020-05-05 16:24:52 +0300

    fix access level for testMake's

--- Source/Test/Address+TestHelper.swift
+++ Source/Test/Address+TestHelper.swift
@@ -4,9 +4,9 @@ import Spry
 import NRequest
 
 extension Address: SpryEquatable {
-    public static func testMake(host: String = "",
-                                endpoint: String? = nil,
-                                queryItems: [String: String] = [:]) -> Address {
+    static func testMake(host: String = "",
+                         endpoint: String? = nil,
+                         queryItems: [String: String] = [:]) -> Address {
         return Address(host: host,
                        endpoint: endpoint,
                        queryItems: queryItems)
