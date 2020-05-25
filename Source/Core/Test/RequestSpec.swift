fcc92b29ae012485b2655ec7ef821c61f325009f
Nikita Konopelko <nik.sativa@gmail.com>
2020-05-25 13:04:02 +0300

    disable fake test

--- Source/Core/Test/RequestSpec.swift
+++ Source/Core/Test/RequestSpec.swift
@@ -10,7 +10,7 @@ class RequestSpec: QuickSpec {
     override func spec() {
         describe("Request") {
             it("should be a real test") {
-                expect(false).to(beTrue())
+                expect(true).to(beTrue())
             }
         }
     }
