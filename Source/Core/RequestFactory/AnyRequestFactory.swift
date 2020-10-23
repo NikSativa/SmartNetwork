b85b37fcd6f4d7240bdd332228aab0a631cbabd4
Nikita Konopelko <nik.sativa@gmail.com>
2020-10-23 09:14:04 +0300

    fix API

--- Source/Core/RequestFactory/AnyRequestFactory.swift
+++ Source/Core/RequestFactory/AnyRequestFactory.swift
@@ -1,35 +1,35 @@
-//import Foundation
-//import UIKit
-//import NCallback
-//
-//final
-//public class AnyRequestFactory<Error: AnyError>: RequestFactory {
-//    private let box: AbstractRequestFactory<Error>
-//
-//    public init<K: RequestFactory>(_ provider: K) where K.Error == Error {
-//        self.box = RequestFactoryBox(provider)
-//    }
-//
-//    public func request<T: CustomDecodable, R: Request>(_ type: T.Type, with parameters: Parameters) -> R where R.Error == Error, R.Response == T.Object {
-//        box.request(type, with: parameters)
-//    }
-//}
-//
-//private class AbstractRequestFactory<Error: AnyError>: RequestFactory {
-//    func request<T: CustomDecodable, R: Request>(_: T.Type, with parameters: Parameters) -> R where R.Error == Error, R.Response == T.Object {
-//        fatalError("abstract needs override")
-//    }
-//}
-//
-//final
-//private class RequestFactoryBox<T: RequestFactory>: AbstractRequestFactory<T.Error> {
-//    private var concrete: T
-//
-//    init(_ concrete: T) {
-//        self.concrete = concrete
-//    }
-//
-//    override func request<T: CustomDecodable, R: Request>(_ type: T.Type, with parameters: Parameters) -> R where R.Error == Error, R.Response == T.Object {
-//        concrete.request(type, with: parameters)
-//    }
-//}
+import Foundation
+import UIKit
+import NCallback
+
+final
+public class AnyRequestFactory<Error: AnyError>: RequestFactory {
+    private let box: AbstractRequestFactory<Error>
+
+    public init<K: RequestFactory>(_ provider: K) where K.Error == Error {
+        self.box = RequestFactoryBox(provider)
+    }
+
+    public func request<T: CustomDecodable>(_ type: T.Type, with parameters: Parameters) -> ResultCallback<T.Object, T.Error> {
+        box.request(type, with: parameters)
+    }
+}
+
+private class AbstractRequestFactory<Error: AnyError>: RequestFactory {
+    func request<T: CustomDecodable>(_: T.Type, with parameters: Parameters) -> ResultCallback<T.Object, T.Error> {
+        fatalError("abstract needs override")
+    }
+}
+
+final
+private class RequestFactoryBox<T: RequestFactory>: AbstractRequestFactory<T.Error> {
+    private var concrete: T
+
+    init(_ concrete: T) {
+        self.concrete = concrete
+    }
+
+    override func request<T: CustomDecodable>(_ type: T.Type, with parameters: Parameters) -> ResultCallback<T.Object, T.Error> {
+        concrete.request(type, with: parameters)
+    }
+}
--- Source/Core/RequestFactory/RequestFactory.swift
+++ Source/Core/RequestFactory/RequestFactory.swift
@@ -1,79 +1,61 @@
-//import Foundation
-//import UIKit
-//import NCallback
-//
-//public protocol RequestFactory: class {
-//    associatedtype Error: AnyError
-//
-//    func request<T: CustomDecodable, R: Request>(_: T.Type, with parameters: Parameters) -> R where R.Error == Error, R.Response == T.Object
-//
-//    /// proxy helpers
-//    func requestIgnorable<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == Ignorable
-//
-//    func request<R: Request>(_: R.Response.Type, with parameters: Parameters) -> R where R.Error == Error, R.Response: Decodable
-//    func request<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response: Decodable
-//
-//    func requestImage<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == UIImage
-//    func requestOptionalImage<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == UIImage?
-//
-//    func requestData<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == Data
-//    func requestOptionalData<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == Data?
-//
-//    func requestAny<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == Any
-//    func requestOptionalAny<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == Any?
-//
-//    /// convert to AnyRequestFactory
-//    func toAny() -> AnyRequestFactory<Error>
-//}
-//
-//public extension RequestFactory {
-//    // MARK - Ignorable
-//    func requestIgnorable<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == Ignorable {
-//        request(IgnorableContent<R.Error>.self, with: parameters)
-//    }
-//
-//    // MARK - Decodable
-//    func request<R: Request>(_: R.Response.Type, with parameters: Parameters) -> R where R.Error == Error, R.Response: Decodable {
-//        request(DecodableContent<R.Response, R.Error>.self, with: parameters)
-//    }
-//
-//    func request<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response: Decodable {
-//        request(DecodableContent<R.Response, R.Error>.self, with: parameters)
-//    }
-//
-//    // MARK - Image
-//    func requestImage<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == UIImage {
-//        request(ImageContent<R.Error>.self, with: parameters)
-//    }
-//
-//    func requestOptionalImage<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == UIImage? {
-//        request(OptionalImageContent<R.Error>.self, with: parameters)
-//    }
-//
-//    // MARK - Data
-//    func requestData<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == Data {
-//        request(DataContent<R.Error>.self, with: parameters)
-//    }
-//
-//    func requestOptionalData<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == Data? {
-//        request(OptionalDataContent<R.Error>.self, with: parameters)
-//    }
-//
-//    // MARK - Any/JSON
-//    func requestAny<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == Any {
-//        request(JSONContent<R.Error>.self, with: parameters)
-//    }
-//
-//    func requestOptionalAny<R: Request>(with parameters: Parameters) -> R where R.Error == Error, R.Response == Any? {
-//        request(OptionalJSONContent<R.Error>.self, with: parameters)
-//    }
-//
-//    // MARK -
-//    func toAny() -> AnyRequestFactory<Error> {
-//        if let self = self as? AnyRequestFactory<Error> {
-//            return self
-//        }
-//
-//        return AnyRequestFactory(self)
-//    }
-//}
+import Foundation
+import UIKit
+import NCallback
+
+public protocol RequestFactory: class {
+    associatedtype Error: AnyError
+
+    func request<T: CustomDecodable>(_: T.Type, with parameters: Parameters) -> ResultCallback<T.Object, T.Error>
+}
+
+public extension RequestFactory {
+    // MARK - Ignorable
+    func requestIgnorable(with parameters: Parameters) -> ResultCallback<Ignorable, Error> {
+        request(IgnorableContent<Error>.self, with: parameters)
+    }
+
+    // MARK - Decodable
+    func request<T: Decodable>(_: T.Type, with parameters: Parameters) -> ResultCallback<T, Error> {
+        request(DecodableContent<T, Error>.self, with: parameters)
+    }
+
+    func request<T: Decodable>(with parameters: Parameters) -> ResultCallback<T, Error> {
+        request(DecodableContent<T, Error>.self, with: parameters)
+    }
+
+    // MARK - Image
+    func requestImage(with parameters: Parameters) -> ResultCallback<UIImage, Error> {
+        request(ImageContent<Error>.self, with: parameters)
+    }
+
+    func requestOptionalImage(with parameters: Parameters) -> ResultCallback<UIImage?, Error> {
+        request(OptionalImageContent<Error>.self, with: parameters)
+    }
+
+    // MARK - Data
+    func requestData(with parameters: Parameters) -> ResultCallback<Data, Error> {
+        request(DataContent<Error>.self, with: parameters)
+    }
+
+    func requestOptionalData(with parameters: Parameters) -> ResultCallback<Data?, Error> {
+        request(OptionalDataContent<Error>.self, with: parameters)
+    }
+
+    // MARK - Any/JSON
+    func requestAny(with parameters: Parameters) -> ResultCallback<Any, Error> {
+        request(JSONContent<Error>.self, with: parameters)
+    }
+
+    func requestOptionalAny(with parameters: Parameters) -> ResultCallback<Any?, Error> {
+        request(OptionalJSONContent<Error>.self, with: parameters)
+    }
+
+    // MARK -
+    func toAny() -> AnyRequestFactory<Error> {
+        if let self = self as? AnyRequestFactory<Error> {
+            return self
+        }
+
+        return AnyRequestFactory(self)
+    }
+}
