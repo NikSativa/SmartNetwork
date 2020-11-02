a37d7f6eb4f2248e9590385841fe517a5989812a
Nikita Konopelko <nik.sativa@gmail.com>
2020-11-02 16:16:14 +0200

    status code: upgrade required

--- Source/Core/Error/StatusCode.swift
+++ Source/Core/Error/StatusCode.swift
@@ -1,16 +1,17 @@
 import Foundation
 
 public enum StatusCode: Error, Equatable {
-    case noContent    // 204
-    case badRequest   // 400
-    case unauthorized // 401
-    case notFound     // 404
-    case timeout      // 408
-    case serverError  // 500
+    case noContent       // 204
+    case badRequest      // 400
+    case unauthorized    // 401
+    case notFound        // 404
+    case timeout         // 408
+    case upgradeRequired // 426
+    case serverError     // 500
     case other(Int)
 }
 
-extension StatusCode: ErrorMapping {
+extension StatusCode {
     public func toInt() -> Int {
         switch self {
         case .noContent:
@@ -23,13 +24,17 @@ extension StatusCode: ErrorMapping {
             return 404
         case .timeout:
             return 408
+        case .upgradeRequired:
+            return 426
         case .serverError:
             return 500
         case .other(let code):
             return code
         }
     }
+}
 
+extension StatusCode: ErrorMapping {
     public static func verify(_ code: Int?) throws {
         guard let code = code else {
             return
@@ -48,6 +53,8 @@ extension StatusCode: ErrorMapping {
             throw notFound
         case 408:
             throw timeout
+        case 426:
+            throw upgradeRequired
         case 500:
             throw serverError
         default:
