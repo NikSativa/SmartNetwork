1743a4dfd246530ca6acff925a28e65e7cbda741
Mykyta Konopelko <nik.sativa@gmail.com>
2020-11-03 15:45:02 +0200

    address refactoring

--- Source/Core/Customization/CustomizedDecodable.swift
+++ Source/Core/Customization/CustomizedDecodable.swift
@@ -3,3 +3,15 @@ import Foundation
 public protocol CustomizedDecodable: Decodable {
     static var decoder: JSONDecoder { get }
 }
+
+extension Array: CustomizedDecodable where Element: CustomizedDecodable {
+    public static var decoder: JSONDecoder {
+        return Element.decoder
+    }
+}
+
+extension Dictionary: CustomizedDecodable where Value: CustomizedDecodable, Key: Decodable {
+    public static var decoder: JSONDecoder {
+        return Value.decoder
+    }
+}
