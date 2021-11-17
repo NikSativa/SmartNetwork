e539b2f3f709efa9540258761a89f70dabfa56f5
Nikita Konopelko <nik.sativa@gmail.com>
2021-11-17 22:08:28 +0200

    revert crthage project

---
+++ NRequest-Carthage/NRequest/NRequest.h
@@ -0,0 +1,18 @@
+//
+//  NRequest.h
+//  NRequest
+//
+//  Created by Максим Казаков on 15.11.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for NRequest.
+FOUNDATION_EXPORT double NRequestVersionNumber;
+
+//! Project version string for NRequest.
+FOUNDATION_EXPORT const unsigned char NRequestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NRequest/PublicHeader.h>
+
+
